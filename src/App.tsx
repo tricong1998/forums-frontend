76e0287079666244cd903dd220f9bc065916a8f9
Cong Nguyen <cong.nguyen@sotatek.com>
2020-12-09 23:26:27 +0700

    feat: home page

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/base/Header";
 import { Switch, Route } from "react-router-dom";
+import { useSelector, shallowEqual } from "react-redux";
+import { ArticleState, Article } from "./components/Article/Article.type";
+import Home from "./components/Home/Home";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header appName='Forums'/>
       <Switch>
-        <Route exact path='/' component={} />
+        <Route exact path='/' component={Home} />
       </Switch>
     </div>
   );
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,11 +2,24 @@ import { ArticleState, Article } from "../Article/Article.type"
 import React from "react"
 import { useSelector, shallowEqual, useDispatch } from "react-redux";
 
-class Home extends React.Component<> {
-  articles: Article[] = useSelector((state: ArticleState) => state.articles, shallowEqual)
+const ArticlePreview = (props: {
+  article: Article
+} )=> {
+  return (<div>
+
+  </div>)
+}
+class Home extends React.Component {
+  const articles: Article[] = useSelector((state: ArticleState) => state.articles, shallowEqual)
   render() {
     return (
-      
+      <div>
+        {this.articles.map(article => {
+          return <ArticlePreview article></ArticlePreview>
+        })}
+      </div>
     )
   }
 }
+
+export default Home
